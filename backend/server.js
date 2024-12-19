39453c409deacc93e1cc6d4ed53b01da6d3c1a0a
Hasitha <hasithapeiris.me@gmail.com>
2024-12-19 11:02:07 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Extract cookie data from HTTP requests
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  console.log(`Server is started at port ${PORT}`);
+});
